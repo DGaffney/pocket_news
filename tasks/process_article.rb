3251ed7706b762f71f04156d7fc20f1ad7781e72
Devin Gaffney <itsme@devingaffney.com>
2015-07-18 02:33:07 -0700

    updates

--- tasks/process_article.rb
+++ tasks/process_article.rb
@@ -4,7 +4,7 @@ class ProcessArticle
   
   def perform(article, username, count)
     Article.from_raw(article, username)
-    if Article.count(username: username)/count.to_f > 0.75
+    if Article.count(username: username)/count.to_f > 0.95
       ["word_counts", "read_unread", "top_terms", "term_network"].each do |stat|
         sr = StatResult.first_or_create(username: username, name: stat)
         sr.value = Stats.new.get(username, stat)
