04abc14e12b8948b615706fc72351f53c282e64b
Devin Gaffney <devingaffney@Devins-MacBook-Pro.local>
2018-10-11 13:15:19 -0400

    updates

--- extensions/array.rb
+++ extensions/array.rb
@@ -113,7 +113,7 @@ class Array
     summary_statistics[:median] = self.compact.median 
     summary_statistics[:mode] = self.mode
     summary_statistics[:mean] = self.average
-    summary_statistics[:standard_deviation] = self.standard_deviation
+    summary_statistics[:standard_deviation] = self.compact.standard_deviation
     summary_statistics[:sum] = self.sum
     summary_statistics[:sample_variance] = self.compact.sample_variance
     summary_statistics[:elbow] = self.elbow
